940d8db29f9de3d3c5c3124e9536ef4956b9af6e
jaber-tebyan <jabertebyan@gmail.com>
2026-01-22 22:33:00 +0300

    feat: add an example prayer time using adhan package

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,43 @@
+import 'package:adhan/adhan.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  final kocaeliCoordinates = Coordinates(40.7625, 29.9175);
+  final params = CalculationMethod.muslim_world_league.getParameters();
+  params.madhab = Madhab.shafi;
+  final prayerTimes = PrayerTimes.today(kocaeliCoordinates, params);
+  runApp(MainApp(prayerTimes: prayerTimes));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final PrayerTimes prayerTimes;
+  const MainApp({super.key, required this.prayerTimes});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Center(child: PrayerTimesWidget(prayerTimes: prayerTimes)),
       ),
+    );
+  }
+}
+
+class PrayerTimesWidget extends StatelessWidget {
+  final PrayerTimes prayerTimes;
+  const PrayerTimesWidget({super.key, required this.prayerTimes});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Prayer Times"),
+        Text("Fajr Prayer: ${prayerTimes.fajr}"),
+        Text("Duhr Prayer: ${prayerTimes.dhuhr}"),
+        Text("Asr Prayer: ${prayerTimes.asr}"),
+        Text("Maghrib Prayer: ${prayerTimes.maghrib}"),
+        Text("Isha Prayer: ${prayerTimes.isha}"),
+      ],
     );
   }
 }
